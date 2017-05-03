19fb373163a2571dd0441c13559599ad5b43bbdc
discopatrick <git@discopatrick.me>
2017-05-03 17:47:28 +0100

    Set up automated tests

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # A web crawler project
 
 To run the tests: `python3.6 -m unittest`
+
+To run the tests automatically upon code change:
+* `npm install -g gulp-cli`
+* `npm install`
+* `gulp watch`
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('test', shell.task([
+  'python3.6 -m unittest'
+]));
+
+gulp.task('watch', function(){
+  gulp.watch(
+    [
+      './**/*.py',
+    ],
+    ['test']
+  )
+})
