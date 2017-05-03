0ee37ddc4b03938340f3ab44d7f820534ba25a75
discopatrick <git@discopatrick.me>
2017-05-03 19:18:55 +0100

    Add test for sitemap output

--- source/web_crawler.py
+++ source/web_crawler.py
@@ -10,3 +10,9 @@ def get_links_from_string(string):
   doc = pq(string)
   links = doc('a')
   return links
+
+def get_link_list_from_links(links):
+  output = '\n'
+  for link in links:
+    output += pq(link).attr['href'] + '\n'
+  return output
