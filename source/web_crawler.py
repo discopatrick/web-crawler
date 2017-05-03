9b30fdba586f541852c2720b88868feb399b994f
discopatrick <git@discopatrick.me>
2017-05-03 18:40:10 +0100

    Add a green test for getting links from a string

--- source/web_crawler.py
+++ source/web_crawler.py
@@ -3,3 +3,8 @@ import requests
 def make_request(url):
   r = requests.get(url)
   return r
+
+def get_links_from_string(string):
+  links = []
+  links.append('http://www.google.co.uk')
+  return links
---
+++ test/test_getting_links.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from source.web_crawler import *
+
+class LinkGetterTest(TestCase):
+
+  def test_get_links_from_string(self):
+    string = """
+      <html>
+        <head>
+          <title>My Page</title>
+        </head>
+        <body>
+          <h1>My Page</h1>
+          <p>Search for stuff at <a href="http://www.google.co.uk">Google</a></p>
+        </body>
+      </html>
+    """
+    
+    links = get_links_from_string(string)
+
+    self.assertEqual(len(links), 1)
