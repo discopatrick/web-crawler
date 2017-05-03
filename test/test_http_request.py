c68dfa2b1aace491441c03303eb1cee64f2b4419
discopatrick <git@discopatrick.me>
2017-05-03 18:15:21 +0100

    Test response body is not empty

--- test/test_http_request.py
+++ test/test_http_request.py
@@ -3,8 +3,14 @@ from source.web_crawler import *
 
 class HttpRequestTest(TestCase):
 
-  def test_http_request(self):
+  def test_http_request_status_code(self):
     url = 'http://www.bbc.co.uk'
     response = make_request(url)
 
     self.assertEqual(response.status_code, 200)
+
+  def test_http_request_body(self):
+    url = 'http://www.bbc.co.uk'
+    response = make_request(url)
+
+    self.assertIsNotNone(response.text)
