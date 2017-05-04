6f1169f00ccfb6a4ea82791d0e6d82b88013ff35
discopatrick <git@discopatrick.me>
2017-05-04 08:56:56 +0100

    Skip tests that hit the network
    We don’t want to hit the network every time we run the unit tests,
    so we’re skipping these tests.

--- test/test_http_request.py
+++ test/test_http_request.py
@@ -1,20 +1,23 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 from source.web_crawler import *
 
 class HttpRequestTest(TestCase):
-
+  
+  @skip
   def test_http_request_status_code(self):
     url = 'http://www.bbc.co.uk'
     response = make_request(url)
 
     self.assertEqual(response.status_code, 200)
 
+  @skip
   def test_http_request_body(self):
     url = 'http://www.bbc.co.uk'
     response = make_request(url)
 
     self.assertIsNotNone(response.text)
 
+  @skip
   def test_bad_url(self):
     url = 'asdf'
     response = make_request(url)
