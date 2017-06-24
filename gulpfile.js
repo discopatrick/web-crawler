d9d1f41877fbb90488ed67970bb2d15eba8dc937
discopatrick <git@discopatrick.me>
2017-06-24 22:26:20 +0100

    Update test command

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('test', shell.task([
-  'python -m unittest'
+  'venv/bin/python -m unittest'
 ]));
 
 gulp.task('flake8', shell.task([
