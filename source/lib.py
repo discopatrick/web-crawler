7f645fab4f301d0346f8c26b18d3ab362ec1def2
discopatrick <git@discopatrick.me>
2017-06-26 22:39:41 +0100

    Add crawl function to Crawler with mock _crawl_url method

--- source/lib.py
+++ source/lib.py
@@ -1,3 +1,6 @@
+import random
+from uuid import uuid4
+
 class Crawler(object):
 
     def __init__(self, start_url):
@@ -12,10 +15,36 @@ class Crawler(object):
         url_obj = Url(url_string)
         self._url_list.append(url_obj)
 
+    def _get_next_uncrawled_url(self):
+        for url_obj in self._url_list:
+            if url_obj.crawled is False:
+                return url_obj
+
+    def _get_random_bool(self):
+        return bool(random.getrandbits(1))
+
+    def _crawl_url(self, url_obj):
+        if self._get_random_bool():
+            self.add_url_as_string('dummy-url-{}'.format(uuid4()))
+        url_obj.crawled = True
+
+    def crawl(self):
+        while True:
+            next = self._get_next_uncrawled_url()
+            if next is not None:
+                self._crawl_url(next)
+            else:
+                break
+
+
 class Url(object):
 
-    def __init__(self, url):
-        self._url = url
+    def __init__(self, url_string):
+        self._url = url_string
+        self.crawled = False
+
+    def __str__(self):
+        return '<Url object - _url: {} - crawled: {}>'.format(self._url, self.crawled)
 
     @property
     def url(self):
