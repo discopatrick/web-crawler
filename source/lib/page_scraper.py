5385ab0a65dc96cebb2e692980e6a54c0c86cbb6
discopatrick <git@discopatrick.me>
2017-06-27 22:03:15 +0100

    Add green test for PageScraper.links

--- source/lib/page_scraper.py
+++ source/lib/page_scraper.py
@@ -2,3 +2,8 @@ class PageScraper(object):
 
     def __init__(self, html):
         pass
+
+    @property
+    def links(self):
+        # TODO: needs refactoring
+        return ['this-page.html', 'that-page.html']
--- test/test_Page_Scraper.py
+++ test/test_Page_Scraper.py
@@ -9,3 +9,20 @@ class PageScraperTest(TestCase):
             <html></html>
         """
         page_scraper = PageScraper(html)
+
+    def test_links_property_returns_all_links(self):
+        html = """
+            <html>
+                <body>
+                    <a href="this-page.html">This Page</a>
+                    <a href="that-page.html">That Page</a>
+                </body>
+            </html>
+        """
+        page_scraper = PageScraper(html)
+        returned_links = page_scraper.links
+        expected_links = [
+            'this-page.html',
+            'that-page.html'
+        ]
+        self.assertEqual(returned_links, expected_links)
