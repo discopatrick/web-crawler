1183434ba490d4e1da6862cb974b688c48d8a034
discopatrick <git@discopatrick.me>
2017-06-27 22:27:13 +0100

    Refactor PageScraper.links

--- source/lib/page_scraper.py
+++ source/lib/page_scraper.py
@@ -1,9 +1,18 @@
+from pyquery import PyQuery as pq
+
 class PageScraper(object):
 
     def __init__(self, html):
-        pass
+        self._html = html
+
+    def _get_links_from_html(self):
+        doc = pq(self._html)
+        anchor_elements = doc('a')
+        anchor_hrefs = []
+        for el in anchor_elements:
+            anchor_hrefs.append(pq(el).attr['href'])
+        return anchor_hrefs
 
     @property
     def links(self):
-        # TODO: needs refactoring
-        return ['this-page.html', 'that-page.html']
+        return self._get_links_from_html()
